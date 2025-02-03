b8c4413b2737b360646a9f5e7d79dfbd94d04fca
pc <j.boyer69003@gmail.com>
2025-02-03 17:55:14 +0100

    ok

--- runsimple.py
+++ runsimple.py
@@ -45,7 +45,12 @@ app = FastAPI(lifespan=lifespan)
 async def init_app():
 
     # Initialisation du moteur de manière asynchrone
-    engine_args = AsyncEngineArgs(model=MODEL_NAME)
+    engine_args = AsyncEngineArgs(model=MODEL_NAME,
+        quantization="awq", dtype="auto"
+        # cpu_offload_gb=10,
+        # gpu_memory_utilization=0.95,
+        #  quantization="fp8",
+    )
     engine =  AsyncLLMEngine.from_engine_args(engine_args)
 
     # Obtention de la configuration du modèle
